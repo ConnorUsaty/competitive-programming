2661ed366909bcd2dd6bbce6ccc57c94e9fd7c2d
ConnorUsaty <connorusaty@gmail.com>
2024-09-20 00:05:42 -0400

    Update template.cpp

--- solutions/template.cpp
+++ solutions/template.cpp
@@ -1,6 +1,21 @@
 // g++-std=c++11-O2-Wall test.cpp-o test
 
-#include <bits/stdc++.h>
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <deque>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <random>
+#include <set>
+#include <string>
+#include <vector>
+typedef long long ll;
+typedef long double ld;
 
 using namespace std; 
 
