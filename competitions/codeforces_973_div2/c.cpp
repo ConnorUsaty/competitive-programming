30f4ed3033e0e4e987555daf91ef5014c9926090
ConnorUsaty <connorusaty@gmail.com>
2024-09-22 12:10:33 -0400

    Upsolve contest problem

--- competitions/codeforces_973_div2/c.cpp
+++ competitions/codeforces_973_div2/c.cpp
@@ -19,12 +19,47 @@ typedef long double ld;
 
 using namespace std; 
 
-// ios::sync_with_stdio(0); 
-// cin.tie(0);
+
+bool ask(string t) {
+    // check if t substr of s
+    cout << "? " << t << endl;
+    int res;
+    cin >> res; // 1 if True, 0 if False
+    return res;
+}
+ 
+void result(string s) {
+    // send final result
+    cout << "! " << s << endl;
+}
 
 
 void solve() {
+    int n;
+    cin >> n;
+    string cur;
 
+    // build up suffix
+    while ((int)cur.size() < n) {
+        if (ask(cur + "0")) {
+            cur += "0";
+        } else if (ask(cur + "1")) {
+            cur += "1";
+        } else {
+            break;
+        }
+    }
+
+    // build up prefix
+    while ((int)cur.size() < n) {
+        if (ask("0" + cur)) {
+            cur = "0" + cur;
+        } else{
+            cur = "1" + cur;
+        }
+    }
+
+    result(cur);
     return;
 }
 
@@ -39,3 +74,4 @@ int main() {
 
     return 0;
 }
+
