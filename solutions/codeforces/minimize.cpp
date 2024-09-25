250f75021ddc507d935a9a70d41d6ade0669869b
ConnorUsaty <connorusaty@gmail.com>
2024-09-24 23:29:37 -0400

    Create minimize.cpp

---
+++ solutions/codeforces/minimize.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h> 
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <deque>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <random>
+#include <set>
+#include <string>
+#include <vector>
+#include <stack>
+
+typedef long long ll;
+typedef long double ld;
+// typedef pair<int,int> pii;
+
+using namespace std; 
+
+
+void solve() {
+    int a, b;
+    cin >> a >> b;
+
+    cout << b-a << "\n";
+    return;
+}
+
+
+int main() { 
+    ios::sync_with_stdio(0); 
+    cin.tie(0);
+
+    int test_cases;
+    cin >> test_cases;
+    while (test_cases--) {
+        solve();
+    }
+
+    return 0;
+}
+
+// 800 Rating
+// Accepted
+
+// c cancels out of the equation, leaving b-a everytime
