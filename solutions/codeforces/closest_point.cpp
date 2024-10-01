78f8b050b014ef813eb01c2e5095e5a0ba89e08a
ConnorUsaty <connorusaty@gmail.com>
2024-09-30 23:46:42 -0400

    Create closest_point.cpp

---
+++ solutions/codeforces/closest_point.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h> 
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <deque>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <random>
+#include <set>
+#include <string>
+#include <vector>
+#include <stack>
+
+typedef long long ll;
+typedef long double ld;
+// typedef pair<int,int> pii;
+
+using namespace std; 
+
+
+void solve() {
+    int n;
+    cin >> n;
+
+    vector<int> a(n);
+    for (int i=0;i<n;++i){
+        cin>>a[i];
+    }
+
+    if (n==2&&(abs(a[0]-a[1])>1)){
+        cout<<"YES\n";
+    } else {
+        cout<<"NO\n";
+    }
+
+    return;
+}
+
+
+int main() { 
+    ios::sync_with_stdio(0); 
+    cin.tie(0);
+
+    int test_cases;
+    cin >> test_cases;
+    while (test_cases--) {
+        solve();
+    }
+
+    return 0;
+}
