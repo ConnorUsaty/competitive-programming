ff172aed7efd26bc8949a1158725f2ba27e8cc08
ConnorUsaty <connorusaty@gmail.com>
2024-10-20 14:48:11 -0400

    codeforces comp 979

---
+++ competitions/codeforces_979_div2/b.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h> 
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <deque>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <random>
+#include <set>
+#include <string>
+#include <vector>
+#include <stack>
+
+typedef long long ll;
+typedef long double ld;
+// typedef pair<int,int> pii;
+
+using namespace std; 
+
+
+void solve() {
+    // f(t) -> only 0 -> = n!, where n = num of zeros
+    // need to keep track of total chars and num of zeros and min score with those 2
+
+    int n;
+    cin >> n;
+
+    string s = "1";
+    s.append(n-1, '0');
+
+    cout << s << "\n";
+    return;
+}
+
+
+int main() { 
+    ios::sync_with_stdio(0); 
+    cin.tie(0);
+
+    int test_cases;
+    cin >> test_cases;
+    while (test_cases--) {
+        solve();
+    }
+
+    return 0;
+}
