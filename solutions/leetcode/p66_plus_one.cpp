e14b72d8877cd6794b3cc1d6d612731a3cc55ad3
ConnorUsaty <connorusaty@gmail.com>
2024-12-07 19:06:09 -0500

    Create p66_plus_one.cpp

---
+++ solutions/leetcode/p66_plus_one.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        digits[digits.size()-1]++;
+
+        for(int i=digits.size()-1;i>0;--i){
+            if(digits[i]==10){
+                digits[i-1]++;
+                digits[i]=0;
+            } else {
+                break;
+            }
+        }
+
+        if(digits[0]==10){
+            digits[0]=1;
+            digits.push_back(0);
+        }
+
+        return digits;
+    }
+};
