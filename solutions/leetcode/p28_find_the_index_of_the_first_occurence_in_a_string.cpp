919ce40cd8d08c4a79f67ac6761e4c2aad7f5dfe
ConnorUsaty <connorusaty@gmail.com>
2024-12-25 15:09:08 -0500

    Update p28_find_the_index_of_the_first_occurence_in_a_string.cpp

--- solutions/leetcode/p28_find_the_index_of_the_first_occurence_in_a_string.cpp
+++ solutions/leetcode/p28_find_the_index_of_the_first_occurence_in_a_string.cpp
@@ -1,15 +1,27 @@
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-        auto pos = haystack.find(needle);
-        if (pos != string::npos) {
-            return pos;
+        // use KMP algorithm O(m+n)
+        int m = (int)needle.size();
+        int n = (int)haystack.size();
+        vector<int> pi(m, 0);
+
+        // create pi table O(m)
+        for(int i=1;i<m;++i){
+            int j=pi[i-1];
+            while(j>0 && needle[j]!=needle[i]) j = pi[j-1];
+            if(j==0 && needle[0]!=needle[i]) pi[i]=0;
+            else pi[i]=j+1;
         }
-        else {
-            return -1;
+
+        // use pi table for search O(n)
+        int j=0;
+        for(int i=0;i<n;++i){
+            while(j>0 && haystack[i]!=needle[j]) j = pi[j-1];
+            if(haystack[i]==needle[j]) j++;
+            if(j==m) return i-j+1;
         }
+
+        return -1;
     }
 };
-
-// Can call find() -> O(nm) worst case
-// Can use KMP -> O(n+m) worst case, where n = length of str, m = length of substr
