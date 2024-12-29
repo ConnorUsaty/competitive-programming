c5a3d8845c3684612d4742d307f94e388af927b0
ConnorUsaty <connorusaty@gmail.com>
2024-12-28 22:38:50 -0500

    Update p219_contains_duplicate_II.cpp

--- solutions/leetcode/p219_contains_duplicate_II.cpp
+++ solutions/leetcode/p219_contains_duplicate_II.cpp
@@ -11,3 +11,6 @@ public:
         return false;
     }
 };
+
+// can also be treated as sliding window problem
+// any problem where you have to check for x within k adjacent elements or a window of k size or within last k elements -> sliding window -> two ptr or deque or prefix sums or cnts
