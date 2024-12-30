92624cbb10cbbf3a3890b36d792e3bd4fe477166
ConnorUsaty <connorusaty@gmail.com>
2024-12-29 22:39:55 -0500

    Update p322_coin_change.cpp

--- solutions/leetcode/p322_coin_change.cpp
+++ solutions/leetcode/p322_coin_change.cpp
@@ -1,16 +1,23 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-        vector<long long> dp(amount+1, INT_MAX);
+        // init dp array with proper values for solution
+        vector<int> dp(amount+1, 1e9);
         dp[0]=0;
-        for(int i=0;i<amount+1;++i){
-            for(int j=0;j<coins.size();++j){
-                if(coins[j]<=i){
+
+        // at each amount check the minimum amount of coins required to reach that amount
+        for(int i=1; i<=amount; ++i){
+            for(int j=0; j<coins.size(); ++j){
+                if(i-coins[j]>=0){
+                    // if possible to get to this amount using this coin
+                    // check if better to use prev solution or use this coin
                     dp[i] = min(dp[i], dp[i-coins[j]]+1);
                 }
             }
         }
-        return (dp[amount]==INT_MAX)?-1:dp[amount];
+
+        // if dp[amount] = init value, then it is not possible to reach that amount with these coins
+        return (dp[amount]==1e9) ? -1 : dp[amount];
     }
 };
 
