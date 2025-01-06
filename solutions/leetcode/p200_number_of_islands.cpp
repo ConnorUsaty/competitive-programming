35dc11ef4890e78ad8a15c79eaeb94111ecc1d5b
ConnorUsaty <connorusaty@gmail.com>
2025-01-05 22:26:12 -0500

    Update p200_number_of_islands.cpp

--- solutions/leetcode/p200_number_of_islands.cpp
+++ solutions/leetcode/p200_number_of_islands.cpp
@@ -25,4 +25,34 @@ public:
         }
         return islands;
     }
+};class Solution {
+private:
+    void dfs(int i, int j, vector<vector<char>>& grid){
+        if(i<0 || i>=grid.size() || j<0 || j>=grid[0].size() || grid[i][j]!='1'){
+            return;
+        }
+
+        grid[i][j] = '#';
+
+        dfs(i+1, j, grid);
+        dfs(i-1, j, grid);
+        dfs(i, j+1, grid);
+        dfs(i, j-1, grid);
+    }
+
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        int ans=0;
+        for(int i=0; i<grid.size(); ++i){
+            for(int j=0; j<grid[0].size(); ++j){
+                if(grid[i][j] == '1'){
+                    dfs(i, j, grid);
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
 };
+
+// traverse grid, performing dfs at each 1 found to mark island as visited and increment ans
