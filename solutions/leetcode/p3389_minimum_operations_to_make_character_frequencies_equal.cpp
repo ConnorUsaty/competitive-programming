d668c47b3e6776f4c0e3f0149ffc901fccd034ad
ConnorUsaty <connorusaty@gmail.com>
2025-01-06 23:41:22 -0500

    Update p3389_minimum_operations_to_make_character_frequencies_equal.cpp

--- solutions/leetcode/p3389_minimum_operations_to_make_character_frequencies_equal.cpp
+++ solutions/leetcode/p3389_minimum_operations_to_make_character_frequencies_equal.cpp
@@ -1,26 +1,47 @@
-#define ll int
-#define FOR(i,a,b)      for(ll i=(a);i<(b);i++)
-
 class Solution {
 public:
     int makeStringGood(string s) {
-        vector<ll> count(26, 0); for (auto c : s) count[c-'a']++;
-        ll mx = 0; for (auto x : count) mx = max(x, mx);
-        ll res = INT_MAX;
-        FOR(target, 1, mx+1) {
-            vector<ll> dp(27, 0);
-            for (ll j = 25; j >=0; j--) {
-                dp[j] = count[j] + dp[j+1]; // delete all 
-                dp[j] = min(dp[j], abs(count[j]-target) + dp[j+1]); // insert or remove to have exactly target
-                ll moveNext = count[j] > target ? count[j]-target : count[j]; // move next
-                if (j+1 < 26 && count[j+1] < target)
-                    dp[j] = min(dp[j], moveNext + max(target-count[j+1]-moveNext, 0) + dp[j+2]);                
-            }
-            res = min(res, dp[0]);
+        vector<int> cnt(26, 0);
+        for(const char& c : s){
+            cnt[c-'a']++;
         }
-        return res;
+
+        int mx = -1;
+        for(const int& n : cnt){
+            mx = max(mx, n);
+        }
+
+        int ans = 1e9;
+        // iterate over all possible final freqs to make s good
+        for(int i=1; i<mx+1; ++i){
+            vector<int> dp(27, 0);
+
+            // iterate over each char in alphabet
+            for(int j=25; j>=0; --j){
+                if(cnt[j]>=i || i-cnt[j]<cnt[j]){
+                    // go to target -> by either adding or deleting
+                    dp[j] = dp[j+1] + abs(cnt[j]-i);
+                } else {
+                    // go to zero -> by deleting all
+                    dp[j] = dp[j+1] + cnt[j];
+                }
+                // deleting from curr and adding to prev -> optimized with swaps
+                if(j<25 && cnt[j+1]<i){
+                    int dels = (i>cnt[j]) ? cnt[j] : cnt[j]-i;
+                    int adds = i-cnt[j+1];
+                    dp[j] = min(dp[j], dp[j+2]+adds+dels-min(dels, adds));
+                }
+            }
+            ans = min(ans, dp[0]);
+        }
+        return ans;
     }
 };
 
-// bottom up will almost always be more time and space efficient than top down with memo
-// however bottom up is normally harder to come up with
+// at each index we have three main choices
+// increase to target
+// decrease to 0
+// swap with adjacent element
+
+// can choose between the first two immediately by checking if closer to target or zero
+// can check for third option after by checking the 
