3d58208eb7287554b3e7c284ec2d96cc40cdf629
ConnorUsaty <connorusaty@gmail.com>
2025-01-15 13:00:05 -0500

    Update p300_longest_increasing_subsequence.cpp

--- solutions/leetcode/p300_longest_increasing_subsequence.cpp
+++ solutions/leetcode/p300_longest_increasing_subsequence.cpp
@@ -1,55 +1,43 @@
-// GREEDY SOLUTION O(nlogn) time
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-        vector<int> sub = {nums[0]};
+        vector<int> best = {nums[0]};
         for(int i=1; i<nums.size(); ++i){
-            if(sub.back()<nums[i]){
-                sub.push_back(nums[i]);
+            if(nums[i]>best[best.size()-1]){
+                best.push_back(nums[i]);
             }
-
-            int l=0, r=sub.size()-1;
-            while(l<=r){
-                int m = l+(r-l)/2;
-
-                if(sub[m]<nums[i]){
-                    l=m+1;
-                } else {
-                    r=m-1;
+            else if(nums[i]<best[best.size()-1]){
+                int l=0, r=best.size()-1;
+                while(l<=r){
+                    int m = l+(r-l)/2;
+                    if(best[m]<nums[i]){
+                        l=m+1;
+                    } else {
+                        r=m-1;
+                    }
                 }
+                best[l] = nums[i];
             }
-            sub[l] = nums[i];
         }
-
-        return (int)sub.size();
+        return (int)best.size();
+        
+        // vector<int> dp(nums.size(), 1);
+        // for(int i=1; i<nums.size(); ++i){
+        //     for(int j=0; j<i; ++j){
+        //         if(nums[j]<nums[i]){
+        //             dp[i] = max(dp[i], dp[j]+1);
+        //         }
+        //     }
+        // }
+        // return *max_element(dp.begin(), dp.end());
     }
 };
 
+// subsequence -> DP
+// dp[i] = LIS ending at [i]
+// LIS doesnt necessarily end on [n], therefore have to return *max_element()
+// O(n^2) time, O(n) space
 
-// DP SOLUTION O(n^2) time
-class Solution {
-public:
-    int lengthOfLIS(vector<int>& nums) {
-        vector<int> dp(nums.size(), 1);
-
-        for(int i=1; i<nums.size(); ++i){
-            for(int j=i-1; j>=0; --j){
-                if (nums[j] < nums[i]) {
-                    dp[i] = max(dp[i], dp[j]+1);
-                }
-            }
-        }
-
-        // since dp[i] is LIS INCLUDING i and not up to i we have to return the max element
-        // instead of return dp[n-1]
-        return *max_element(dp.begin(), dp.end());
-    }
-};
-
-// subsequence, should immediately signal backtracking / dp
-// since we only need to track a trait of the subsequences (the longest increast ss)
-// this should immediately signal dp
-
-// let dp[i] store the LIS up until and including [i]
-// at each i move backwards until u find the first element strictly smaller than nums[i]
-// dp[i] = dp[j] + 1, where nums[j] is the first < nums[i]
+// Go across only track best subsequence if > last just push_back
+// if < find where it should be and update ss -> should replace first num > than curr
+// O(nlogn) because we go across once for n, then logn to find where it should go if neccessary
