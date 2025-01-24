5f4e55e1d62261e70d59cce70852c32e17c6556e
ConnorUsaty <connorusaty@gmail.com>
2025-01-23 23:16:52 -0500

    Update p76_minimum_window_substring.cpp

--- solutions/leetcode/p76_minimum_window_substring.cpp
+++ solutions/leetcode/p76_minimum_window_substring.cpp
@@ -1,58 +1,34 @@
-
-
-// min window with cnt1 == cnt2
-
-// expand window keeping track of cnt and ans string until we find a valid answer
-// shorten window as much as possible while window remains valid
-// when answer no longer valid restart from step 1 (expanding window)
-// repeat until gone through entire s, return min len valid ans
-
-// Inititial answer:
-// would work except 'A' != 'a'
 class Solution {
-private:
-    bool validWindow(int (&cnt1)[26], int (&cnt2)[26]){
-        for(int k=0;k<26;++k){
-            if(cnt1[k]>cnt2[k]) return false;
-        }
-        return true;
-    }
 public:
     string minWindow(string s, string t) {
-        int l=0,r=s.length()+1;
-        int cnt1[26] = {0};
-        int cnt2[26] = {0};
+        unordered_map<char,int> cnt_t;
+        for(const char& c : t) {
+            cnt_t[c]++;
+        }
+        int missing = (int)cnt_t.size();
 
-        for(const char& c : t) cnt1[tolower(c)-'a']++;
-
-        int i=0,j=0;
-        while(j<s.length()){
-            while(!validWindow(cnt1,cnt2) && j<s.length()){
-                cnt2[tolower(s[j])-'a']++;
+        unordered_map<char,int> cnt_s;
+        int i=0, j=0, l=0, r=1e9;
+        while(i<(int)s.length()){
+            cnt_s[s[i]]++;
+            if(cnt_s[s[i]] == cnt_t[s[i]]) missing--;
+            while(!missing) {
+                if(i-j < r-l){
+                    l = j;
+                    r = i;
+                }
+                if(cnt_s[s[j]] == cnt_t[s[j]]) missing++;
+                cnt_s[s[j]]--;
                 j++;
             }
-            while(validWindow(cnt1,cnt2)){
-                if(j-i<r-l){
-                    l=i;
-                    r=j;
-                }
-                cnt2[tolower(s[i])-'a']--;
-                i++;
-            }
+            i++;
         }
-        if(r-l==(int)s.length()+1) return "";
-
-        string ans;
-        for(int k=l;k<r;++k){
-            ans.push_back(s[k]);
-        }
-        return ans;
+        return (r==1e9) ? "" : s.substr(l, r-l+1);
     }
 };
 
-// min window with cnt1 == cnt2
-
-// expand window keeping track of cnt and ans string until we find a valid answer
-// shorten window as much as possible while window remains valid
-// when answer no longer valid restart from step 1 (expanding window)
-// repeat until gone through entire s, return min len valid ans
+// expand window until t included, then shorten window as much as possible while t still in s
+// can just keep l and r ptrs and use substr(l, r-l+1) at end
+// can do map.size() to get size of maps
+// simple sliding window problem, expand window until requirements meet, then decrease window as much as possible while still meeting requirements, keep track of best ans throughout
+// changing to unordered_map greatly decreased runtime
