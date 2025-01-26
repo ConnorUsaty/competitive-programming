3a86b9286354967aaba50b72d1fbd9dabd7533fa
ConnorUsaty <connorusaty@gmail.com>
2025-01-25 19:47:13 -0500

    Create p91_decode_ways.cpp

---
+++ solutions/leetcode/p91_decode_ways.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int numDecodings(string s) {
+        int p = 1, pp, n = s.size();
+        for(int i=n-1;i>=0;i--) {
+            int cur = s[i]=='0' ? 0 : p;
+            if(i<n-1 && (s[i]=='1'||s[i]=='2'&&s[i+1]<'7')) cur+=pp;
+            pp = p;
+            p = cur;
+        }
+        return s.empty()? 0 : p;   
+    }
+};
+
+
+// can build up solution similar to climbing stairs
+// think about how you would solve problem with 1,2 or 3 indexes, see if same pattern can continue on
+// think about possible edge cases, i.e. '0's in this problem
