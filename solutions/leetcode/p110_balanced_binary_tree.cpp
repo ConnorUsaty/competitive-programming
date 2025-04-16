e45a4f62da119f5f6b0fb3671cc34a5ba3118e27
ConnorUsaty <connorusaty@gmail.com>
2025-04-16 19:38:14 -0400

    Update p110_balanced_binary_tree.cpp

--- solutions/leetcode/p110_balanced_binary_tree.cpp
+++ solutions/leetcode/p110_balanced_binary_tree.cpp
@@ -9,31 +9,37 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+ 
 class Solution {
 private:
-    void solve(TreeNode* t, bool& ans, int& h) {
-        if(!t || !ans) {
-            return;
-        }
-        int l=0, r=0;
-        solve(t->left,ans,l);
-        solve(t->right,ans,r);
+    int height(TreeNode* t, bool& ans){
+        if(!ans || t==nullptr) return 0;
 
-        h = max(l,r)+1;
-        if(abs(l-r)>1){
+        int lh = height(t->left, ans);
+        int rh = height(t->right, ans);
+
+        if(max(lh,rh) - min(lh,rh) > 1){
             ans = false;
         }
+
+        return max(lh,rh) + 1;
     }
 
 public:
     bool isBalanced(TreeNode* root) {
         bool ans = true;
-        int h = 0;
-        solve(root,ans,h);
+        int h = height(root, ans);
         return ans;
     }
 };
-
-// can pass address of h and ans so that they are passed by reference and values are updated without returning
-// need to init variables before passing when passing by reference
