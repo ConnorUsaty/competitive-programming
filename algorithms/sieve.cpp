cb951be848aacf812c6f4f97c0d1ece6a6b2daf3
ConnorUsaty <connorusaty@gmail.com>
2025-06-28 14:22:23 -0700

    fix: sieve algo

--- algorithms/sieve.cpp
+++ algorithms/sieve.cpp
@@ -1,12 +1,28 @@
 /* Generate all primes from 2 to n in O(n*log(log(n))) */
 
-int n;
-vector<int> prime(n+1, 1);
-prime[0] = prime[1] = 0;
-for(int i=2; i<=n; ++i){
-    if(prime[i] && i*i<=n){
-        for(int j=i*i; j<=n; j+=i){
-            prime[i]=0;
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n = 200;
+    vector<int> prime(n+1, 1);
+    prime[0] = prime[1] = 0;
+    for(int i=2; i<=n; ++i){
+        if(prime[i] && i*i<=n){
+            for(int j=i*i; j<=n; j+=i){
+                prime[j]=0;
+            }
         }
     }
+
+    for(int i=100; i<n; ++i){
+        if(prime[i]){
+            cout << i << ' ';
+        }
+    }
+    cout << "\n";
+
+    return 0;
 }
