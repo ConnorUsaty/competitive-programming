382d7126cc15b319e8f0002c8b80817e1d701895
ConnorUsaty <connorusaty@gmail.com>
2025-07-10 21:59:38 -0700

    Update p70_climbing_stairs.cpp

--- solutions/leetcode/p70_climbing_stairs.cpp
+++ solutions/leetcode/p70_climbing_stairs.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        if(n<=3) return n;
-
-        int p1=3, p2=2;
-        for(int i=4; i<=n; ++i){
+        int p1 = 1; // step - 1
+        int p2 = 1; // step - 2
+        for(int i=2; i<=n; ++i){
+            int t = p1;
             p1 += p2;
-            p2 = p1-p2;
+            p2 = t;
         }
         return p1;
     }
 };
 
-// can build on subproblems
-// i.e. ways to get to step 4 = (ways to step 3) + (ways to step 2)
+// you can get to every step from step-1 or step-2
+// therefore num ways to get to step = # ways step-1 + # ways step-2
