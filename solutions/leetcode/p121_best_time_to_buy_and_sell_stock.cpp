fa61e86155bc8324a5f06137244e3cf99f6f1fef
ConnorUsaty <connorusaty@gmail.com>
2025-07-10 22:04:24 -0700

    Update p121_best_time_to_buy_and_sell_stock.cpp

--- solutions/leetcode/p121_best_time_to_buy_and_sell_stock.cpp
+++ solutions/leetcode/p121_best_time_to_buy_and_sell_stock.cpp
@@ -1,17 +1,14 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int min_element=1e9;
-        int max_diff=0;
-
-        for (const int& n : prices) {
-            min_element = min(min_element, n);
-            max_diff = max(max_diff, n-min_element);
+        int ans=0, mn = INT_MAX;
+        for(auto& p : prices){
+            mn = min(mn, p);
+            ans = max(ans, p-mn);
         }
-
-        return max_diff;
+        return ans;
     }
 };
 
-// Can just use greedy checking at each index if its a better buy or sell point
-// const int& improves memory / speed slightly in for loop -> & can be used when deep copy not required, const can be used when read only required, i.e. no modifying of val n
+// keep track of min (best time to buy)
+// at each [i] check if we have found a better buy or a better sell
