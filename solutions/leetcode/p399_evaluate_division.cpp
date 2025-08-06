322c0a959d637ef9c32fa9b7ee65278b619e43b5
ConnorUsaty <connorusaty@gmail.com>
2025-08-05 21:34:07 -0700

    Update p399_evaluate_division.cpp

--- solutions/leetcode/p399_evaluate_division.cpp
+++ solutions/leetcode/p399_evaluate_division.cpp
@@ -39,3 +39,51 @@ public:
 // can be used for hierarchy orderings (topological sort)
 // can be used for unit conversions (weighted edges)
 // can be used for any kind of interaction of relationships
+
+
+
+// BFS SOLUTION:
+
+class Solution {
+private:
+    double bfs(string& start, string& end, unordered_map<string,vector<pair<string,double>>>& adj){
+        queue<pair<string,double>> q;
+        unordered_set<string> visited;
+        q.push({start, 1.0});
+        visited.insert(start);
+
+        while(!q.empty()){
+            auto [u, d] = q.front();
+            q.pop();
+
+            if(u == end) return d;
+
+            for(auto& [v, w] : adj[u]){
+                if(visited.find(v) == visited.end()){
+                    q.push({v, d*w});
+                    visited.insert(v);
+                }
+            }
+        }
+        return -1.0;
+    }
+
+public:
+    vector<double> calcEquation(vector<vector<string>>& equations, vector<double>& values, vector<vector<string>>& queries) {
+        unordered_map<string,vector<pair<string,double>>> adj;
+        for(int i=0; i<equations.size(); ++i){
+            adj[equations[i][0]].push_back({equations[i][1], values[i]});
+            adj[equations[i][1]].push_back({equations[i][0], 1/values[i]});
+        }
+
+        vector<double> ans;
+        for(auto& q : queries){
+            if(adj.find(q[0]) == adj.end() || adj.find(q[1]) == adj.end()) {
+                ans.push_back(-1.0);
+            } else {
+                ans.push_back(bfs(q[0], q[1], adj));
+            }
+        }
+        return ans;
+    }
+};
