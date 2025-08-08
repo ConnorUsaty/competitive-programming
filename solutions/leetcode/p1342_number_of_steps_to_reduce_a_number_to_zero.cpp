5ad1aa9bdc0271d99600b80909a5314f5bae2eb5
ConnorUsaty <connorusaty@gmail.com>
2025-08-07 22:07:12 -0700

    Update p1342_number_of_steps_to_reduce_a_number_to_zero.cpp

--- solutions/leetcode/p1342_number_of_steps_to_reduce_a_number_to_zero.cpp
+++ solutions/leetcode/p1342_number_of_steps_to_reduce_a_number_to_zero.cpp
@@ -1,15 +1,11 @@
 class Solution {
 public:
     int numberOfSteps(int num) {
-        int ans=0;
-        while(num){
-            if(num&1){
-                num--;
-            }else{
-                num >>= 1;
-            }
-            ans++;
-        }
-        return ans;
+        if(num==0) return num;
+        
+        return (31-__builtin_clz(num)) + (__builtin_popcount(num));
     }
 };
+
+// ans = (MSB index + 1) + (num of bits set - 1)
+// ans = (MSB index) + (num of bits set)
