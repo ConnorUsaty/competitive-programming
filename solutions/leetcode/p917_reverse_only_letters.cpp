59fe25d9c90ba98a6370ef110db67565d11bb270
ConnorUsaty <connorusaty@gmail.com>
2025-08-16 08:28:00 +0200

    Create p917_reverse_only_letters.cpp

---
+++ solutions/leetcode/p917_reverse_only_letters.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string reverseOnlyLetters(string s) {
+        int l=0, r=s.size()-1;
+
+        while(l<r){
+            while(l<r && !isalpha(s[l])){
+                l++;
+            }
+            while(l<r && !isalpha(s[r])){
+                r--;
+            }
+            swap(s[l], s[r]);
+            l++;
+            r--;
+        }
+        return s;
+    }
+};
+
+// isalpha(char c) c++ util function that checks if c is a letter (upper or lower case)
