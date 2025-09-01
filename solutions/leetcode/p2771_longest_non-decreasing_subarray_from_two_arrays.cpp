bb6936d094b06f32a0138ffb7590877f917bac6a
ConnorUsaty <connorusaty@gmail.com>
2025-09-01 18:43:40 -0400

    Update p2771_longest_non-decreasing_subarray_from_two_arrays.cpp

--- solutions/leetcode/p2771_longest_non-decreasing_subarray_from_two_arrays.cpp
+++ solutions/leetcode/p2771_longest_non-decreasing_subarray_from_two_arrays.cpp
@@ -1,25 +1,38 @@
 class Solution {
 public:
     int maxNonDecreasingLength(vector<int>& nums1, vector<int>& nums2) {
-        int n = (int)nums1.size();
+        const int n = (int)nums1.size();
 
         int ans = 1;
-        vector<vector<int>> dp(2, vector<int>(n,1));
-        for(int i=1; i<n; ++i){
-            if(nums1[i]>=nums2[i-1]){
-                dp[0][i] = dp[1][i-1] + 1;
-            }
+        vector<int> dp1(n, 1);
+        vector<int> dp2(n, 1);
+        for(int i=1; i<n; ++i) {
             if(nums1[i]>=nums1[i-1]){
-                dp[0][i] = max(dp[0][i], dp[0][i-1]+1);
+                dp1[i] = max(dp1[i], dp1[i-1] + 1);
+            }
+            if(nums1[i]>=nums2[i-1]){
+                dp1[i] = max(dp1[i], dp2[i-1] + 1);
             }
             if(nums2[i]>=nums2[i-1]){
-                dp[1][i] = dp[1][i-1] + 1;
+                dp2[i] = max(dp2[i], dp2[i-1] + 1);
             }
             if(nums2[i]>=nums1[i-1]){
-                dp[1][i] = max(dp[1][i], dp[0][i-1]+1);
+                dp2[i] = max(dp2[i], dp1[i-1] + 1);
             }
-            ans = max({ans, dp[0][i], dp[1][i]});
+            ans = max({ans, dp1[i], dp2[i]});
         }
         return ans;
     }
 };
+
+// remember to look for space optimizations on DP problems
+// i.e. here we only ever look back 1 idx so we should only need O(1) space not O(n)
+
+// max len >= subarray
+
+// always choose min value for [0]
+// this logic doesn't continue for all i indexes though
+// keep 2 dp arrays
+// dp1[i] = max len subarray ending on nums1[i]
+// dp2[i] = max len subarray ending on nums2[i]
+// this covers both possible options and thus our ans is the max that we encounter
