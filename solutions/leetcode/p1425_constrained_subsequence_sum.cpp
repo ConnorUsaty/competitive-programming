e1a322084854fc48d7620bc7958b658f32446f38
ConnorUsaty <connorusaty@gmail.com>
2025-09-04 11:24:22 -0400

    Update p1425_constrained_subsequence_sum.cpp

--- solutions/leetcode/p1425_constrained_subsequence_sum.cpp
+++ solutions/leetcode/p1425_constrained_subsequence_sum.cpp
@@ -3,26 +3,33 @@ public:
     int constrainedSubsetSum(vector<int>& nums, int k) {
         const int n = (int)nums.size();
 
-        int ans = INT_MIN;
         vector<int> dp(n);
         deque<int> q;
-        for(int i=0; i<n; ++i){
-            if(!q.empty() && i-q.front() > k) {
+        for(int i=0; i<n; ++i) {
+            while(!q.empty() && i-q.front() > k) {
                 q.pop_front();
             }
-
-            dp[i] = (!q.empty() ? dp[q.front()] : 0) + nums[i];
-
+            int prev = (q.empty() ? 0 : dp[q.front()]);
+            dp[i] = max(nums[i], prev + nums[i]);
             while(!q.empty() && dp[q.back()] < dp[i]) {
                 q.pop_back();
             }
-            if(dp[i] > 0) {
-                q.push_back(i);
-            }
+            q.push_back(i);
         }
         return *max_element(dp.begin(), dp.end());
     }
 };
+
+// build up a non-empty subseq where adj elements idx differ by <= k
+// return MAX sum possible from a subseq built that way
+
+// obviously we build up a subseq by checking what the max possible sum we could have at [i] is
+// to get max possible sum when we can only choose from last K elements, we just need to know the max in the last K
+// we can keep track of this with a monotonic q
+
+// then for each idx we simply set dp[i] = max({dp[q.front()] + nums[i], nums[i]});
+
+// return *max_element(dp.begin(), dp.end());
 
 // subsequence does NOT always mean dp -> that would give us O(n*k) time which will TLE
 
