2ea2a7c441d05a09e55c82c8367625b1c6b68a00
ConnorUsaty <connorusaty@gmail.com>
2025-10-14 21:08:01 -0400

    Create p330_patching_array.cpp

---
+++ solutions/leetcode/p330_patching_array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minPatches(vector<int>& nums, int n) {
+        int ans = 0, i = 0;
+        long long miss = 1;
+        while(miss <= n) {
+            if (i<nums.size() && nums[i]<=miss) {
+                miss += nums[i++];
+            } else {
+                miss <<= 1;
+                ans++;
+            }
+        }
+        return ans;
+
+    }
+};
